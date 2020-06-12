2c4615235bbc1b638bc290e21794d2d442f13797
Serge Ivlev <Liho@ro.ru>
2020-06-12 19:42:36 +0300

    refactor(genDiff.js)

--- src/genDiff.js
+++ src/genDiff.js
@@ -2,45 +2,43 @@ import _ from 'lodash';
 import parse from './parsers.js';
 
 const render = (coll) => {
-  const result = coll.flatMap(({
-    type, name, value, oldValue, newValue,
-  }) => {
-    switch (type) {
-      case 'unchange':
-        return `    ${name}: ${value}`;
-      case 'modifed':
-        return [`  - ${name}: ${oldValue}`, `  + ${name}: ${newValue}`];
-      case 'deleted':
-        return `  - ${name}: ${value}`;
-      default:
-        return `  + ${name}: ${value}`;
-    }
-  }).join('\n');
+  const result = coll
+    .flatMap(({
+      type, name, value, oldValue, newValue,
+    }) => {
+      switch (type) {
+        case 'unchanged':
+          return `    ${name}: ${value}`;
+        case 'changed':
+          return [`  - ${name}: ${oldValue}`, `  + ${name}: ${newValue}`];
+        case 'deleted':
+          return `  - ${name}: ${value}`;
+        default:
+          return `  + ${name}: ${value}`;
+      }
+    }).join('\n');
   return `{\n${result}\n}`;
 };
 
-export default (path1, path2) => {
-  const f1 = parse(path1);
-  const f2 = parse(path2);
+const genDiff = (path1, path2) => {
+  const file1 = parse(path1);
+  const file2 = parse(path2);
+  const union = _.union(Object.keys(file1), Object.keys(file2));
 
-  const add = Object.entries(f2).flatMap(([key, val]) => {
-    if (!_.has(f1, key)) {
-      return { name: key, value: val, type: 'added' };
-    }
+  const deleted = union.filter((key) => !_.has(file2, key))
+    .map((key) => ({ name: key, value: file1[key], type: 'deleted' }));
+  const added = union.filter((key) => !_.has(file1, key))
+    .map((key) => ({ name: key, value: file2[key], type: 'added' }));
+  const unchanged = union
+    .filter((key) => file1[key] === file2[key])
+    .map((key) => ({ name: key, value: file1[key], type: 'unchanged' }));
+  const changed = union
+    .filter((key) => (_.has(file2, key) && _.has(file1, key)) && file1[key] !== file2[key])
+    .map((key) => ({
+      name: key, oldValue: file1[key], newValue: file2[key], type: 'changed',
+    }));
 
-    return [];
-  });
-  const other = Object.entries(f1).map(([key, val]) => {
-    if (f2[key] === val) {
-      return { name: key, value: val, type: 'unchange' };
-    }
-    if (_.has(f2, key) && f2[key] !== val) {
-      return {
-        name: key, oldValue: val, type: 'modifed', newValue: f2[key],
-      };
-    }
-    return { name: key, value: val, type: 'deleted' };
-  });
-
-  return render([...other, ...add]);
+  return render([...unchanged, ...changed, ...added, ...deleted]);
 };
+
+export default genDiff;
